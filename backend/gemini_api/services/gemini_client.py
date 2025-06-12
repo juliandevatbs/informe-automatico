6c24f7e8548e7ae6ca203119cd2ae9e8f3254491
juliandevatbs <julian.criollo@advancedtbs.com>
2025-06-12 13:35:52 -0500

    Connection with the gemini client succesfully

---
+++ backend/gemini_api/services/gemini_client.py
@@ -0,0 +1,51 @@
+"""
+    
+    This file configures the connection to the gemini api
+
+    
+"""
+
+
+import os
+from google import genai
+from dotenv import load_dotenv
+
+
+class GeminiClient:
+    
+    def __init__(self):
+        
+        load_dotenv()
+        
+        self.api_gemini_key = os.getenv('GOOGLE_GEMINI_API_KEY')
+        
+        if not self.api_gemini_key:
+            
+            print("Google gemini api key is not found")
+            
+        self._connect()
+        
+    def _connect(self):
+        
+        """
+        
+            Connection with gemini method
+            
+        """
+        
+        
+        try:
+            
+            self.client = genai.Client(api_key=self.api_gemini_key)
+            print("Successfully connected to Gemini API")
+        
+        except Exception as ex:
+            
+            print(f"Error {ex}")
+            
+            
+            
+if __name__ == '__main__':
+    
+    gemini_client = GeminiClient()
+            
